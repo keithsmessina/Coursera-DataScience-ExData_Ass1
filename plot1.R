cdab350495cc39460fab7d7a1ef59330c9649e46
keithsmessina <keith.s.messina@gmail.com>
2015-07-11 09:40:33 -0400

    Functions almost complete
    Functions completed without comments.

--- plot1.R
+++ plot1.R
@@ -1,4 +1,5 @@
-rawdata <- read.table(unz("exdata_data_household_power_consumption.zip",
+plotGraph <- function(){
+  rawdata <- read.table(unz("exdata_data_household_power_consumption.zip",
                           "household_power_consumption.txt"), col.names =c(
                             "Date","Time","Global_active_power","Global_reactive_power",
                             "Voltage", "Global_intensity", "Sub_metering_1",
@@ -9,9 +10,13 @@ rawdata <- read.table(unz("exdata_data_household_power_consumption.zip",
                               as.double(character()), as.double(character()),
                               as.double(character()), as.double(character())),
                       na.strings="?",sep = ";", stringsAsFactors = FALSE, header=TRUE)
-filteredData <- rawdata[as.Date(rawdata[,"Date"], format="%d/%m/%Y")
+  filteredData <- rawdata[as.Date(rawdata[,"Date"], format="%d/%m/%Y")
                         == as.Date("01/02/2007", format="%d/%m/%Y")|
                           as.Date(rawdata[,"Date"], format="%d/%m/%Y") ==
                           as.Date("02/02/2007", format="%d/%m/%Y"),]
-hist(filteredData$Global_active_power, ylim=c(0,1200), col="red", include.lowest=TRUE,
-     xlab="Global Active Power (kilowatts)", main="Global Active Power")
+  library(datasets)
+  png("plot1.png", width=480, height=480, units="px")
+  hist(filteredData$Global_active_power, ylim=c(0,1200), col="red", include.lowest=TRUE,
+       xlab="Global Active Power (kilowatts)", main="Global Active Power")
+  dev.off()
+  }
